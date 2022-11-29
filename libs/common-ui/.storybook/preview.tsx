200dcf137774e6c3e6a0aff9dbb4ee65be733ce7
Chen Jing Hsuan <shane@jgallop.com>
2022-11-30 03:01:20 +0800

    20221129 Shane update --> feat. Import font in storybook's preview.tsx config to render the custom font properly;

--- libs/common-ui/.storybook/preview.tsx
+++ libs/common-ui/.storybook/preview.tsx
@@ -1,5 +1,9 @@
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from '@sso-platform/theme';
+import '@fontsource/noto-sans-tc/300.css';
+import '@fontsource/noto-sans-tc/400.css';
+import '@fontsource/noto-sans-tc/500.css';
+import '@fontsource/noto-sans-tc/700.css';
 
 export const decorators = [
   // eslint-disable-next-line @typescript-eslint/ban-types
